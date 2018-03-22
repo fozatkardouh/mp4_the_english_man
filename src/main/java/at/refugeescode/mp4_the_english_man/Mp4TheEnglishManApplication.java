04a02bf7c7d025f6d326e4971d12f30b388c3034
Fozat Kardouh <fozatkardouh@gmail.com>
2018-03-22 10:46:15 +0100

    added project structure

---
+++ src/main/java/at/refugeescode/mp4_the_english_man/Mp4TheEnglishManApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp4_the_english_man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp4TheEnglishManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp4TheEnglishManApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp4_the_english_man/Mp4TheEnglishManApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp4_the_english_man;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp4TheEnglishManApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
